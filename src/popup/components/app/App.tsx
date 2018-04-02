8702bd6caf7435ee5560044069c2989117e3d1e2
brandon <brandondyer64@gmail.com>
2018-04-02 14:06:23 -0700

    Added a form to extension button window

--- src/popup/components/app/App.tsx
+++ src/popup/components/app/App.tsx
@@ -1,13 +1,37 @@
-import React from "react";
+import React from 'react'
 
-import style from "./App.scss";
+import style from './App.scss'
 
 export class App extends React.Component<{}, {}> {
 	render() {
 		return (
 			<main className={style.container}>
-				Hi there!
+				<div className={style.title}>r/CircleOfTrust</div>
+				<div className={style.content}>
+					<p>
+						Welcome to our bot net of sneks! You are now a slave to
+						the will of the people of Reddit!
+					</p>
+					<p>
+						While you don't technically have any rights here in the
+						land of the free, you may enter a circle's url and key
+						to convert other likeminded individuals.
+					</p>
+					<form className={style.form}>
+						<input
+							type="text"
+							name="circle_url"
+							placeholder="URL"
+						/>
+						<input
+							type="text"
+							name="circle_key"
+							placeholder="Key"
+						/>
+						<input type="submit" name="submit" value="Request" />
+					</form>
+				</div>
 			</main>
-		);
+		)
 	}
 }
