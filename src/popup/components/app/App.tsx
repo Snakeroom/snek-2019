0a8fd02bf421fc6ceb70285b2938a192ac4e4c2e
brandon <brandondyer64@gmail.com>
2018-04-02 19:56:31 -0700

    Fixed fetching

--- src/popup/components/app/App.tsx
+++ src/popup/components/app/App.tsx
@@ -1,11 +1,29 @@
-import React from "react";
+import React from 'react'
 
-import style from "./App.scss";
+import style from './App.scss'
 
 export class App extends React.Component<{}, {}> {
 	state = {
-		circlesJoined: 0
-	};
+		circlesJoined: 0,
+		url: '',
+		key: ''
+	}
+
+	handleSubmit(e) {
+		e.preventDefault
+		console.log('fetching')
+		fetch('https://api.sneknet.com/request-circle', {
+			method: 'POST',
+			headers: {
+				Accept: 'application/json',
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				url: this.state.url,
+				key: this.state.key
+			})
+		})
+	}
 
 	render() {
 		return (
@@ -21,21 +39,29 @@ export class App extends React.Component<{}, {}> {
 						land of the free, you may enter a circle's url and key
 						to convert other likeminded individuals.
 					</p>
-					<form
-						method="post"
-						action="https://api.sneknet.com/request-circle"
-						className={style.form}
-					>
-						<input type="text" name="url" placeholder="URL" />
-						<input type="text" name="key" placeholder="Key" />
+					<form onSubmit={this.handleSubmit} className={style.form}>
+						<input
+							type="text"
+							placeholder="URL"
+							onChange={e => {
+								this.setState({ url: e.target.value })
+							}}
+						/>
+						<input
+							type="text"
+							placeholder="Key"
+							onChange={e => {
+								this.setState({ key: e.target.value })
+							}}
+						/>
 						<span className={style.asmCount}>
-							You've assimilated {this.state.circlesJoined}{" "}
+							You've assimilated {this.state.circlesJoined}{' '}
 							Circles
 						</span>
-						<input type="submit" name="submit" value="Request" />
+						<input type="submit" value="Request" />
 					</form>
 				</div>
 			</main>
-		);
+		)
 	}
 }
