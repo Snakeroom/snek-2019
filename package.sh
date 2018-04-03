5cee3ab53ba6797a48168008d22047d909fdbd56
Oden <ops991923@gmail.com>
2018-04-02 18:17:29 -0700

    Fix pkg script

--- package.sh
+++ package.sh
@@ -4,7 +4,5 @@ mkdir -p builds/$VERSION
 cd dist
 zip -r ../builds/$VERSION/out.zip ./*
 cd ../builds/$VERSION
-mv out.zip $VERSION.zip
-chromium --disable-gpu --pack-extension=$(readlink -f ../../dist) --pack-extension-key=$(readlink -f ../../key.pem)
-mv ../../dist.crx $VERSION.crx
+mv out.zip $VERSION.zips
 cd ../..
