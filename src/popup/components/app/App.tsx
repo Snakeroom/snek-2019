8a528bcc5d6d04f8e51ecf3169b92d8b7bd6a8e3
brandon <brandondyer64@gmail.com>
2018-04-02 17:07:38 -0700

    Pushes circle request to server

--- src/popup/components/app/App.tsx
+++ src/popup/components/app/App.tsx
@@ -6,7 +6,7 @@ export class App extends React.Component<{}, {}> {
 	render() {
 		return (
 			<main className={style.container}>
-				<div className={style.title}>r/CircleOfTrust</div>
+				<div className={style.title}>r/CircleofTrust</div>
 				<div className={style.content}>
 					<p>
 						Welcome to our bot net of sneks! You are now a slave to
@@ -17,17 +17,13 @@ export class App extends React.Component<{}, {}> {
 						land of the free, you may enter a circle's url and key
 						to convert other likeminded individuals.
 					</p>
-					<form className={style.form}>
-						<input
-							type="text"
-							name="circle_url"
-							placeholder="URL"
-						/>
-						<input
-							type="text"
-							name="circle_key"
-							placeholder="Key"
-						/>
+					<form
+						method="post"
+						action="https://api.sneknet.com/request-circle"
+						className={style.form}
+					>
+						<input type="text" name="url" placeholder="URL" />
+						<input type="text" name="key" placeholder="Key" />
 						<span className={style.asmCount}>
 							You've assimilated :: Circles
 						</span>
