ad827bbb2ec32737b9af2fc0cc0d29c95a749848
Creatable <creatable@creatable-mac.local>
2019-04-02 18:14:42 -0400

    Make the game more fun

--- package.sh
+++
@@ -1,8 +0,0 @@
-VERSION=$(jq -r ".version" src/assets/static/manifest.json)
-
-mkdir -p builds/$VERSION
-cd dist
-zip -r ../builds/$VERSION/out.zip ./*
-cd ../builds/$VERSION
-mv out.zip $VERSION.zip
-cd ../..
